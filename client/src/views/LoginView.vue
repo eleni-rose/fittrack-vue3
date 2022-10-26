9fdafa4aab62b65adf3908c542941ff4aaddc21b
kelpo <eloonku@gmail.com>
2022-10-26 01:48:19 -0400

    1.1.0

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<template>
+    <main>
+      <div class="columns">
+        <div class="column is-one-third">First column</div>
+        <div class="column is-one-third">Second column</div>
+        <div class="column is-one-third">Third column</div>
+      </div>
+    </main>
+  </template>
+  
