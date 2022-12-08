487d324ef61a07a0699c1d2be5ffb557cbaa66e7
kelpo <eloonku@gmail.com>
2022-12-08 04:55:19 -0500

    MongoDB and Mongoose setup

--- server/index.js
+++ server/index.js
@@ -1,37 +1,25 @@
 const { triggerAsyncId } = require('async_hooks');
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+
 const express = require('express')
 const app = express()
 
 const mongoose = require('mongoose');
+const Post = require('./models/post');
 
-mongoose.connect('mongodb://localhost:27017/fit_track', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-}
-)
+mongoose.connect('mongodb://localhost:27017/fittrack', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("connected to database")
+    })
+    .catch(err => {
+        console.log("error connecting")
+        console.log(err)
+    })
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("connected to database");
-});
-
-//const activityController = require('./controllers/activity'); // always have requires at the top of the file
-
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
-
-app.use((req, res, next) => {
-    res.setHeader('FitTrack');
-})
-
-/// req is request, res is response. 200 is information. 201 is created item. 300 is a redirect.
-// 400 is a bad request (client side). 500 is a server side issue.
+//const activityController = require('./controllers/activity');
 
 app.use('/', express.static('./client/dist'));
-
-////
 
 app.use((err, req, res, next) => {
     console.log(err);
@@ -41,22 +29,25 @@ app.use((err, req, res, next) => {
     });
 })
 
-//// If server is hanging most likely there is an issue with calling res/next/req.
-
 app
 .get('/', (req, res) => {
     res.status(200).send('hello');
 })
 //.use('api/activity', activityController) 
 
-////
-
 app.get('*', (req, res) => {
     res.sendFile('index.html', {root: './client/dist'});
 })
 
-////
-
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+/* If server is hanging most likely there is an issue with calling res/next/req.
+    req is request.
+    res is response. 
+    200 is information. 
+    201 is created item. 
+    300 is a redirect.
+    400 is a bad request (client side). 
+    500 is a server side issue. */
